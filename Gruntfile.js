38e6f482ca48cb202c9317400e21a7fdc95fe6ed
Nelson Kuang <410374308@qq.com>
2015-05-17 23:02:03 +0800

    Create Gruntfile.js

---
+++ Gruntfile.js
@@ -0,0 +1,26 @@
+module.exports = function (grunt) {
+    grunt.initConfig({
+        uglify: {
+            build: {
+                src: 'js/css3clock.js',
+                dest: 'js/css3clock.min.js'
+            }
+        },
+        cssmin: {
+            target: {
+                files: [{
+                    expand: true,
+                    cwd: 'css',
+                    src: ['*.css', '!*.min.css'],
+                    dest: 'css',
+                    ext: '.min.css'
+                }]
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.registerTask('default', ['uglify', 'cssmin']);
+
+};
